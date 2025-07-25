b1ba055a73b1e3316fde4c8edced5e0157176f72
NajeebSayyed <najeebsayyed777@gmail.com>
2025-07-25 17:10:00 +0530

    Working of Document Scanner

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Image } from 'react-native'
+import DocumentScanner from 'react-native-document-scanner-plugin'
 
 const App = () => {
+
+  const [scannedImage, setScannedImage] = useState();
+
+  const scanDocument = async () => {
+    // start the document scanner
+    const { scannedImages } = await DocumentScanner.scanDocument()
+  
+    // get back an array with scanned image file paths
+    if (scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0])
+    }
+  }
+
+  useEffect(() => {
+    // call scanDocument on load
+    scanDocument()
+  }, []);
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+      <Image
+      resizeMode="contain"
+      style={{ width: '100%', height: '100%' }}
+      source={{ uri: scannedImage }}
+    />
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
